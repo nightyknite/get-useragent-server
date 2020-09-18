283bcce8397e9ef1a15cc1f483ef5ebf5b042f86
nightyknite <nightyknite@gmail.com>
2020-09-18 23:17:08 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "net/http"
+  "github.com/labstack/echo"
+  "github.com/labstack/echo/middleware"
+)
+
+func main() {
+  // Echo instance
+  e := echo.New()
+
+  // Middleware
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  // Routes
+  e.GET("/", hello)
+
+  // Start server
+  e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+  ua := c.Request().Header.Get("User-Agent")
+  return c.String(http.StatusOK, ua)
+}
+
+
